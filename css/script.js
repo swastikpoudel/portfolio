54c77bfa68d5638c96dac81c3c412805d08e4bfd
swastikpoudel <swastik.poudel.2@gmail.com>
2025-12-18 14:54:11 +0545

    index changed

---
+++ css/script.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // stop page reload
+
+  const formData = new FormData(form);
+
+  const data = {
+    name: formData.get("name"),
+    phone: formData.get("phone"),
+    message: formData.get("message"),
+  };
+
+  try {
+    const res = await fetch("http://localhost:5000/api/client", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+
+    if (res.ok) {
+      alert("Message sent successfully 🚀");
+      form.reset();
+    } else {
+      alert(result.error || "Something went wrong");
+    }
+  } catch (err) {
+    alert("Server not reachable");
+    console.error(err);
+  }
+});
